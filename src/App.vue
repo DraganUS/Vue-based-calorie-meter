7bd41d1317ae432f32e76bab26f4231184b4983b
DraganUS <usic.d.dragan@gmail.com>
2019-01-31 00:57:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  background: linear-gradient(205deg, rgba(99, 212, 4, 0.92) 0%, rgba(232, 255, 0, 0.92) 77%);
+  height: 1000px;
+  width: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="helloWorld">
+    <div id="counterContainer">
+      <div id="top">
+        <i class="fas fa-heartbeat"></i>
+        <h3>Vue.je Calorie Couner</h3>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+div#counterContainer  {
+  border-radius: 1em;
+  background: #fff;
+  width: 746px;
+  min-height: 471px;
+  overflow: hidden;
+}
+div#top{
+  position: relative;
+  background: #019688;
+  width: 100%;
+  height: 100px;
+  text-align: center;
+  overflow: hidden;
+}
+div#top i{
+  position: absolute;
+  top: -8px;
+  left: 2px;
+  font-size: 131px;
+}
+div#top h3{
+  color: #fff;
+  font-weight: 400;
+  font-size: 42px;
+  margin: 0;
+  margin-top: 21px;
+}
+
+</style>
